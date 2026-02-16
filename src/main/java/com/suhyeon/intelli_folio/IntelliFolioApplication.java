260234a45b4473a871f07bd0f680521a390bc5d3
오수현 <ohsuhyeon@osuhyeon-ui-MacBookPro.local>
2026-02-16 15:49:36 +0900

    init

---
+++ src/main/java/com/suhyeon/intelli_folio/IntelliFolioApplication.java
@@ -0,0 +1,13 @@
+package com.suhyeon.intelli_folio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliFolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliFolioApplication.class, args);
+    }
+
+}
