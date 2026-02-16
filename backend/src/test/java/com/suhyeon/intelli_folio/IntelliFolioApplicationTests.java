92ff4340e747755d4df017239ea4d87665ad4d54
오수현 <ohsuhyeon@osuhyeon-ui-MacBookPro.local>
2026-02-16 16:01:05 +0900

    init: move all files to backend dir

--- backend/src/test/java/com/suhyeon/intelli_folio/IntelliFolioApplicationTests.java
+++ backend/src/test/java/com/suhyeon/intelli_folio/IntelliFolioApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IntelliFolioApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
